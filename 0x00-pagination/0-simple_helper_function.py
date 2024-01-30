9e21c958b17d16847c8240013b045198803b69bf
Serinajefthas <serina.jefthas@gmail.com>
2024-01-30 11:40:41 +0000

    add task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,11 +2,12 @@
 """ Method on pagination in python"""
 import math
 import itertools
+from typing import Tuple
 
 
-def index_range(page, page_size):
-    """Function returns tuple size 2 w/ start and end index corresp to range indexes to return"""
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Function returns tuple size of 2 w/ start and end index
+    corresp to range indexes to return"""
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
-
-
+    return start_index, end_index
