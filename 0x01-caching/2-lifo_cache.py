53c208eed4c12659a33afb44af6fb8b55fbff82f
Serinajefthas <serina.jefthas@gmail.com>
2024-01-31 23:10:07 +0000

    fix lifo algorithm

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
 """LIFO cache module"""
 
 
+from collections import OrderedDict
 from base_caching import BaseCaching
 
 
@@ -11,13 +11,15 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """initialises fifo cache instance"""
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """Assigns item value to key in cache_data dictionary"""
         if key is not None and item is not None:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last = self.cache_data.popitem(True)
-                print(f"DISCARD: {last}")
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 >= BaseCaching.MAX_ITEMS:
+                    lastkey, _ = self.cache_data.popitem(True)
+                print(f"DISCARD: {lastkey}")
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=True)
 
