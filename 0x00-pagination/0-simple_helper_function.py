a2f97903a4b987e70e78141b4d9cc54b6322ca9d
Serinajefthas <serina.jefthas@gmail.com>
2024-02-02 21:22:24 +0000

    add task 1 and 2

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,5 +9,5 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """Function returns tuple size of 2 w/ start and end index
     corresp to range indexes to return"""
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    end_index = (start_index) + page_size
     return start_index, end_index
